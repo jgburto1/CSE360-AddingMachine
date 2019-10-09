5d27ed25d979213287fe6399fa0aed22f2b78a29
jgburto1 <44927752+jgburto1@users.noreply.github.com>
2019-10-08 21:36:37 -0700

    Version 1
    Initial commented version of the second assignment

---
+++ AddingMachine.java
@@ -0,0 +1,61 @@
+package cse360assign2;
+
+/**
+ * @author Jacqueline Burton
+ * Class id: CSE360 - Monday
+ * Assignment: 2
+ * Version: 1.0
+ */
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructs the AddingMachine object setting total to zero
+	 */
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Accesses the total variable, not fully implemented
+	 * @return 0
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds a value to the adder, currently unimplemented
+	 * @param value is the value added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a value from the adder object, currently unimplemented
+	 * @param value is the value subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Returns a string that records the history of operations, not implemented 
+	 * @return ""
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Clears the total, unimplemented
+	 */
+	public void clear() {
+	
+	}
+}
